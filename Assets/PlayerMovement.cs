70233b82d961e1cef15cc9e274d2f3b4c97e9aa1
Canbyy <122677933+Canbyy@users.noreply.github.com>
2026-03-30 17:47:33 +0200

    animations
    animations

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -3,10 +3,11 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed = 5f;
-    private Rigidbody2D rb;
-    private Vector2 moveInput;
-    private Animator animator;
+    public float moveSpeed = 5f;
+    public int facingDirection = 0;
+    public Rigidbody2D rb;
+    public Vector2 moveInput;
+    public Animator animator;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -22,27 +23,23 @@ public class PlayerMovement : MonoBehaviour
         float horizontal = Input.GetAxisRaw("Horizontal"); // Get horizontal input (A/D or Left/Right)
         float vertical = Input.GetAxisRaw("Vertical"); // Get vertical input (W/S or Up/Down)
 
-        animator.SetFloat("horizontal", Mathf.Abs(horizontal); // Update the "moveX" parameter in the animator to control horizontal movement animations
+        if (horizontal > 0 && transform.localScale.x < 0 ||
+            horizontal < 0 && transform.localScale.x > 0)
+        {
+            Flip();
+        }
+
+        animator.SetFloat("horizontal", Mathf.Abs(horizontal)); // Update the "moveX" parameter in the animator to control horizontal movement animations
         animator.SetFloat("vertical", Mathf.Abs(vertical)); // Update the "moveY" parameter in the animator to control vertical movement animations
 
 
         rb.velocity = new Vector2(horizontal * moveSpeed, vertical * moveSpeed); // Set the velocity of the Rigidbody2D based on input and move speed
     }
 
-
-    public void Move(InputAction.CallbackContext context)
+    void Flip()
     {
-        animator.SetBool("isWalking", true); // Set the "isMoving" parameter in the animator based on whether the player is moving
-
-        if (context.canceled)
-        {
-            animator.SetBool("isWalking", false);
-            animator.SetFloat("LastInputX", moveInput.x);
-            animator.SetFloat("LastInputY", moveInput.y);
-        }
-        moveInput = context.ReadValue<Vector2>(); // Read the movement input from the player
-        animator.SetFloat("InputX", moveInput.x); // Set the "moveX" parameter in the animator to control horizontal movement animations
-        animator.SetFloat("InputY", moveInput.y); // Set the "moveY" parameter in the animator to control vertical movement animations
+        facingDirection *= 1;
+        transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
     }
 
 }
