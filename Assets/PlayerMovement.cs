ce50f32767546237f1f84433bc61da705ec05892
Canbyy <122677933+Canbyy@users.noreply.github.com>
2026-04-01 23:06:05 +0200

    changes
    ik heb een nieuwe character toegevoegd en de oude weg gedaan met nieuwe animaties en nu in progress om interactieve deuren te maken.

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -3,7 +3,7 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed = 5f;
+    [SerializeField] public float moveSpeed = 5f;
     public int facingDirection = 0;
     public Rigidbody2D rb;
     public Vector2 moveInput;
@@ -30,7 +30,7 @@ public class PlayerMovement : MonoBehaviour
         }
 
         animator.SetFloat("horizontal", Mathf.Abs(horizontal)); // Update the "moveX" parameter in the animator to control horizontal movement animations
-        animator.SetFloat("vertical", Mathf.Abs(vertical)); // Update the "moveY" parameter in the animator to control vertical movement animations
+        animator.SetFloat("vertical", (vertical)); // Update the "moveY" parameter in the animator to control vertical movement animations
 
 
         rb.velocity = new Vector2(horizontal * moveSpeed, vertical * moveSpeed); // Set the velocity of the Rigidbody2D based on input and move speed
