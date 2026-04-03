87331ec04d3b38321d129826bd4edb4b07401994
Canbyy <122677933+Canbyy@users.noreply.github.com>
2026-04-03 21:16:24 +0200

    Add inventory system assets and move scripts
    Introduce a new inventory system and related assets: add inventory & save controllers (InventoryController, InventorySaveData, SaveController, SaveData), item handling scripts (ItemDictionary, ItemDragHandler, ItemPickupUIController, PlayerItemCollector, Slot), prefabs (Item, Medipack Variant, Slot), and UI/sprite assets (UI_16x16 sprite sheet, Medipack.png, WaterPot.png). Also reorganize existing gameplay scripts into Assets/Scripts and update SampleScene accordingly. These changes implement item pickup/dragging, saving inventory state, and tidy the project structure.

---
+++ Assets/SaveController.cs
@@ -0,0 +1,46 @@
+using System.IO;
+using Unity.Cinemachine;
+using UnityEngine;
+
+public class SaveController : MonoBehaviour
+{
+    private string saveLocation;
+    private InventoryController inventoryController;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        saveLocation = Path.Combine(Application.persistentDataPath, "saveData.json");
+        inventoryController = FindAnyObjectByType<InventoryController>();
+
+        LoadGame();
+    }
+    public void SaveGame()
+    {
+        SaveData saveData = new SaveData
+        {
+            playerPosition = GameObject.FindGameObjectWithTag("Player").transform.position,
+            mapBoundary = FindAnyObjectByType<CinemachineConfiner2D>().BoundingShape2D.name,
+            inventorySaveData = inventoryController.GetInventorySaveData()
+        };
+
+        File.WriteAllText(saveLocation, JsonUtility.ToJson(saveData));
+    }
+    public void LoadGame()
+    {
+        if (File.Exists(saveLocation))
+        {
+            SaveData saveData = JsonUtility.FromJson<SaveData>(File.ReadAllText(saveLocation));
+            
+            GameObject.FindGameObjectWithTag("Player").transform.position = saveData.playerPosition;
+            
+            FindAnyObjectByType<CinemachineConfiner2D>().BoundingShape2D = GameObject.Find(saveData.mapBoundary).GetComponent<PolygonCollider2D>();
+
+            inventoryController.SetInventoryItems(saveData.inventorySaveData);
+        }
+        else
+        {
+            SaveGame(); 
+        }
+
+    }
+}
